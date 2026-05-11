511d8362baf940758dbda3ab25ce9f4984a45104
Harshit Kumar <84062856+Harshit6650@users.noreply.github.com>
2026-05-11 21:39:33 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // If you are not using a custom domain, and your repository name is "Amravati-Partners",
+  // you might need to uncomment and set the basePath and assetPrefix:
+  // basePath: "/Amravati-Partners",
+  // assetPrefix: "/Amravati-Partners",
 };
 
 export default nextConfig;
